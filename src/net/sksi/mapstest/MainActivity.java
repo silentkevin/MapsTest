85e7ad024187b052c69e7dee597a89608ab1bc76
Kevin Brown <silentkevin@gmail.com>
2013-01-25 17:41:56 -0500

    making it work with API level 10

--- src/net/sksi/mapstest/MainActivity.java
+++ src/net/sksi/mapstest/MainActivity.java
@@ -8,9 +8,11 @@ import android.location.Location;
 import android.os.Bundle;
 import android.app.Activity;
 import android.os.IBinder;
+import android.support.v4.app.FragmentActivity;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -20,7 +22,7 @@ import net.sksi.mapstest.util.Utils;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
     static final LatLng HAMBURG = new LatLng(53.558, 9.927);
     static final LatLng KIEL = new LatLng(53.551, 9.993);
     private GoogleMap map;
@@ -58,16 +60,9 @@ public class MainActivity extends Activity {
     protected void onResume() {
         super.onResume();
 
-        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
-                .getMap();
-        Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
-                .title("Hamburg"));
-        Marker kiel = map.addMarker(new MarkerOptions()
-                .position(KIEL)
-                .title("Kiel")
-                .snippet("Kiel is cool")
-                .icon(BitmapDescriptorFactory
-                        .fromResource(R.drawable.ic_launcher)));
+        map = ((SupportMapFragment)this.getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG).title("Hamburg"));
+        Marker kiel = map.addMarker(new MarkerOptions().position(KIEL).title("Kiel").snippet("Kiel is cool").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher)));
 
         this.panMapToLocation(this.latLngToLocation(HAMBURG));
     }
